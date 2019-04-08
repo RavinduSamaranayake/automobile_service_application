548323c00a92cac452f4dd18cf559eadbda5146c
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-08 15:22:56 +0530

    working with google maps

--- android/app/src/main/java/com/reactnativestarter/MainApplication.java
+++ android/app/src/main/java/com/reactnativestarter/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativestarter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new RNFirebasePackage(),
             new RNFirebaseAuthPackage(),
             new VectorIconsPackage(),
--- src/modules/gallery/GalleryViewContainer.js
+++ src/modules/gallery/GalleryViewContainer.js
@@ -22,63 +22,257 @@
 //   }),
 // )(GalleryScreen);
 
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ImageBackground,
+// import React from 'react';
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   ImageBackground,
   
-} from 'react-native';
+// } from 'react-native';
 
-import { fonts, colors } from '../../styles';
+// import { fonts, colors } from '../../styles';
  
 
-class GalleryViewContainer extends React.Component {
+// class GalleryViewContainer extends React.Component {
 
    
  
  
-render(){
-  return (
-   <ImageBackground 
-       source={require('../../../assets/images/background.png')}
-       style={styles.container}
-    > 
-    <View style={styles.textContainer}>
-        <Text style={styles.availableText}>Asistant Service</Text>
+// render(){
+//   return (
+//    <ImageBackground 
+//        source={require('../../../assets/images/background.png')}
+//        style={styles.container}
+//     > 
+//     <View style={styles.textContainer}>
+//         <Text style={styles.availableText}>Asistant Service</Text>
          
-      </View>
+//       </View>
       
-    </ImageBackground>
-  );
-}
+//     </ImageBackground>
+//   );
+// }
 
-}
+// }
 
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     paddingHorizontal: 30,
+//     paddingVertical: 50,
+//     justifyContent: 'space-around',
+     
+//   },
+   
+//   availableText: {
+//     color: colors.white,
+//     fontFamily: fonts.primaryRegular,
+//     fontSize: 40,
+//     marginVertical: 3,
+//   },
+//   textContainer: {
+//     alignItems: 'center',
+//   },
+  
+// });
+
+// export default GalleryViewContainer
+
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Platform,
+  PermissionsAndroid
+} from "react-native";
+import MapView, {
+  Marker,
+  AnimatedRegion,
+  Polyline,
+  PROVIDER_GOOGLE
+} from "react-native-maps";
+// import haversine from "haversine";
+
+// const LATITUDE = 29.95539;
+// const LONGITUDE = 78.07513;
+const LATITUDE_DELTA = 0.009;
+const LONGITUDE_DELTA = 0.009;
+const LATITUDE = 6.931970;
+const LONGITUDE = 79.857750;
+
+class GalleryViewContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: LATITUDE,
+      longitude: LONGITUDE,
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {},
+      coordinate: new AnimatedRegion({
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: 0,
+        longitudeDelta: 0
+      })
+    };
+  }
+  // static navigationOptions = {
+  //   header: null
+  // }
+
+  componentDidMount() {
+    const { coordinate } = this.state;
+
+    this.requestCameraPermission();
+
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        const { routeCoordinates, distanceTravelled } = this.state;
+        const { latitude, longitude } = position.coords;
+
+        const newCoordinate = {
+          latitude,
+          longitude
+        };
+        console.log({ newCoordinate });
+
+        if (Platform.OS === "android") {
+          if (this.marker) {
+            this.marker._component.animateMarkerToCoordinate(
+              newCoordinate,
+              500
+            );
+          }
+        } else {
+          coordinate.timing(newCoordinate).start();
+        }
+
+        this.setState({
+          latitude,
+          longitude,
+          routeCoordinates: routeCoordinates.concat([newCoordinate]),
+          distanceTravelled:
+            distanceTravelled + this.calcDistance(newCoordinate),
+          prevLatLng: newCoordinate
+        });
+      },
+      error => console.log(error),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000,
+        distanceFilter: 10
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
+  getMapRegion = () => ({
+    latitude: this.state.latitude,
+    longitude: this.state.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA
+  });
+
+  calcDistance = newLatLng => {
+    const { prevLatLng } = this.state;
+    //return haversine(prevLatLng, newLatLng) || 0;
+    return 10;
+  };
+
+  requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: "Location Access Permission",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the camera");
+      } else {
+        console.log("Camera permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  render() {
+    return ( 
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          showUserLocation
+          followUserLocation
+          loadingEnabled
+          region={this.getMapRegion()}
+        >
+          <Polyline coordinates={this.state.routeCoordinates} strokeWidth={5} />
+          <Marker.Animated
+            ref={marker => {
+              this.marker = marker;
+            }}
+            coordinate={this.state.coordinate}
+          />
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.bubble, styles.button]}>
+            <Text style={styles.bottomBarContent}>
+              {parseFloat(this.state.distanceTravelled).toFixed(2)} km
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  },
+  bubble: {
     flex: 1,
-    alignItems: 'center',
-    paddingHorizontal: 30,
-    paddingVertical: 50,
-    justifyContent: 'space-around',
-     
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20
   },
-   
-  availableText: {
-    color: colors.white,
-    fontFamily: fonts.primaryRegular,
-    fontSize: 40,
-    marginVertical: 3,
+  latlng: {
+    width: 200,
+    alignItems: "stretch"
   },
-  textContainer: {
-    alignItems: 'center',
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10
   },
-  
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 20,
+    backgroundColor: "transparent"
+  }
 });
 
-export default GalleryViewContainer
-
-
+export default GalleryViewContainer;
