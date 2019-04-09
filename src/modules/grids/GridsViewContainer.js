14ac7c15286c35693aaabd3af5a2c2826ad3a269
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-09 19:32:24 +0530

    successfully fetch items from item json

--- src/modules/grids/GridsViewContainer.js
+++ src/modules/grids/GridsViewContainer.js
@@ -132,130 +132,242 @@
 //   withState('data', 'setData', listData),
 // )(GridView);
 
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  ImageBackground,
-  ActivityIndicator
+
+
+
+
+// import React from 'react';
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   ImageBackground,
+//   ActivityIndicator
   
-} from 'react-native';
+// } from 'react-native';
 
-import { fonts, colors } from '../../styles';
+// import { fonts, colors } from '../../styles';
  
-//import {fetchItems} from '../../../constants/api'
+// //import {fetchItems} from '../../../constants/api'
  
  
 
-class GridsViewContainer extends React.Component {
+// class GridsViewContainer extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      isLoading: true,
-      dataSource: null,
-    }
-  }
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       isLoading: true,
+//       dataSource: null,
+//     }
+//   }
 
-  componentDidMount(){
+//   componentDidMount(){
 
-    return fetch(`http://shan-motors.herokuapp.com/api/auth/get-list-of-users`)//fetch(`https://facebook.github.io/react-native/movies.json`)
-           .then((response) => response.json()) //covert to the json object
-           .then((responsJson) => {
+//     return fetch(`http://shan-motors.herokuapp.com/api/auth/get-list-of-users`)//fetch(`https://facebook.github.io/react-native/movies.json`)
+//            .then((response) => response.json()) //covert to the json object
+//            .then((responsJson) => {
 
-             this.setState({
-               isLoading:false,
-               dataSource: responsJson.users,//responsJson.movies, //array of the movies
-             })
-           })
-    .catch((error) => {
-      console.log(error)
-    });
-  }
-  // static defaultProps = {
-  //   fetchItems
-  // }
+//              this.setState({
+//                isLoading:false,
+//                dataSource: responsJson.users,//responsJson.movies, //array of the movies
+//              })
+//            })
+//     .catch((error) => {
+//       console.log(error)
+//     });
+//   }
+//   // static defaultProps = {
+//   //   fetchItems
+//   // }
 
-  // state = {
-  //   loading: false,
-  //   items: []
-  // }
+//   // state = {
+//   //   loading: false,
+//   //   items: []
+//   // }
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const data = await this.props.fetchItems();
-  //   setTimeout(() => this.setState({ loading: false, items: data.items }), 2000);
-  //   //this.setState({ loading: false, items: data.items });
-  // }
+//   // async componentDidMount() {
+//   //   this.setState({ loading: true });
+//   //   const data = await this.props.fetchItems();
+//   //   setTimeout(() => this.setState({ loading: false, items: data.items }), 2000);
+//   //   //this.setState({ loading: false, items: data.items });
+//   // }
 
 
    
  
  
-render(){
-  if (this.state.isLoading) {
-    return (
-      <View style={styles.container}>
-        <ActivityIndicator/>
-      </View>
-    )
-  }
-  else{
-    let movies = this.state.dataSource
-    // let movies = this.state.dataSource.map((val,key)=>{
-    //   return <View key = {key} style={styles.item}>
-    //              <Text>{val.name} is {val.username}</Text>
-    //          </View>
-    // })
-  return (
-   <ImageBackground 
-       source={require('../../../assets/images/background.png')}
-       style={styles.container}
-    > 
-    <View style={styles.textContainer}>
-        {/* <Text style={styles.availableText}>Our Services and booking</Text>
-        {this.state.items.map((item, i) => (
+// render(){
+//   if (this.state.isLoading) {
+//     return (
+//       <View style={styles.container}>
+//         <ActivityIndicator/>
+//       </View>
+//     )
+//   }
+//   else{
+//     let movies = this.state.dataSource
+//     // let movies = this.state.dataSource.map((val,key)=>{
+//     //   return <View key = {key} style={styles.item}>
+//     //              <Text>{val.name} is {val.username}</Text>
+//     //          </View>
+//     // })
+//   return (
+//    <ImageBackground 
+//        source={require('../../../assets/images/background.png')}
+//        style={styles.container}
+//     > 
+//     <View style={styles.textContainer}>
+//         {/* <Text style={styles.availableText}>Our Services and booking</Text>
+//         {this.state.items.map((item, i) => (
          
-          <Text key={i}>{item.name}</Text>
-        ))}
-          */}
-          {movies}
-      </View>
+//           <Text key={i}>{item.name}</Text>
+//         ))}
+//           */}
+//           {movies}
+//       </View>
       
-    </ImageBackground>
-  );
-}
-}
+//     </ImageBackground>
+//   );
+// }
+// }
 
-}
+// }
 
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     paddingHorizontal: 30,
+//     paddingVertical: 50,
+//     justifyContent: 'space-around',
+     
+//   },
+   
+//   availableText: {
+//     color: colors.white,
+//     fontFamily: fonts.primaryRegular,
+//     fontSize: 40,
+//     marginVertical: 3,
+//   },
+//   textContainer: {
+//     alignItems: 'center',
+//   },
+//   item: {
+//     flex: 1,
+//     alignSelf: 'stretch',
+//   }
+  
+// });
+
+// export default GridsViewContainer
+
+
+
+import React, { Component } from 'react';
+
+import { StyleSheet, FlatList, Text, View, Alert, ActivityIndicator, Platform} from 'react-native';
+
+export default class Project extends Component {
+  
+  constructor(props)
+  {
+
+    super(props);
+
+    this.state = { 
+    isLoading: true
+  }
+  }
+
+  componentDidMount() {
+    
+       return fetch('http://shan-motors.herokuapp.com/api/inventory/ViewItemList')//fetch('https://reactnativecode.000webhostapp.com/FruitsList.php')
+         .then((response) => response.json())
+         .then((responseJson) => {
+           this.setState({
+             isLoading: false,
+             dataSource: responseJson
+           }, function() {
+             // In this block you can do something with new state.
+           });
+         })
+         .catch((error) => {
+           console.error(error);
+         });
+     }
+
+FlatListItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#607D8B",
+        }}
+      />
+    );
+  }
+
+  GetFlatListItem (item) {
+   
+  Alert.alert(item.itemName,"item qty is "+item.quantity+"\nitem price is "+item.sellingPrice+"\nitem brand name is "+item.brandName);
+
+  }
+
+
+  render() {
+
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+    return (
+
+<View style={styles.MainContainer}>
+  
+       <FlatList
+       
+          data={ this.state.dataSource }
+          
+          ItemSeparatorComponent = {this.FlatListItemSeparator}
+
+          // renderItem={({item}) => <Text style={styles.FlatListItemStyle} onPress={this.GetFlatListItem.bind(this, item.fruit_name)}> {item.fruit_name} </Text>}
+          renderItem={({item}) => <Text style={styles.FlatListItemStyle} onPress={this.GetFlatListItem.bind(this, item)}> {item.itemName} </Text>}
+
+          keyExtractor={(item, index) => index}
+          
+         />
+    
+    
+</View>
+            
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    paddingHorizontal: 30,
-    paddingVertical: 50,
-    justifyContent: 'space-around',
-     
+
+MainContainer :{
+
+justifyContent: 'center',
+flex:1,
+margin: 10,
+paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+
+},
+
+FlatListItemStyle: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
-   
-  availableText: {
-    color: colors.white,
-    fontFamily: fonts.primaryRegular,
-    fontSize: 40,
-    marginVertical: 3,
-  },
-  textContainer: {
-    alignItems: 'center',
-  },
-  item: {
-    flex: 1,
-    alignSelf: 'stretch',
-  }
-  
+
 });
 
-export default GridsViewContainer
-
+ 
