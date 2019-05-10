8d224101e262cbf4bd1347dcb35803996fd92d31
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-05-10 18:04:42 +0530

    create device storage for jwt authentication

---
+++ .history/src/services/deviceStorage_20190510180158.js
@@ -0,0 +1,8 @@
+
+import { AsyncStorage } from 'react-native';
+
+const deviceStorage = {
+    // our AsyncStorage functions will go here :)
+};
+
+export default deviceStorage;
---
+++ .history/src/services/deviceStorage_20190510180318.js
@@ -0,0 +1,8 @@
+
+import { AsyncStorage } from 'react-native';
+
+const deviceStorage = {
+    
+};
+
+export default deviceStorage;
