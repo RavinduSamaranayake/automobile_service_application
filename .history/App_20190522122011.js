f4271aa9618438dd8af6eabc1ddb7d2e024a2015
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-05-22 23:22:46 +0530

    working with local notification

---
+++ .history/App_20190522122011.js
@@ -0,0 +1,37 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import { PersistGate } from 'redux-persist/integration/react';
+import { colors } from './src/styles';
+import {ViewPushNotification} fr
+
+import { store, persistor } from './src/redux/store';
+
+import AppView from './src/modules/AppView';
+
+export default function App() {
+  return (
+    // <Provider store={store}>
+    //   <PersistGate
+    //     loading={
+    //       <View style={styles.container}>
+    //         <ActivityIndicator color={colors.red} />
+    //       </View>
+    //     }
+    //     persistor={persistor}
+    //   >
+    //     <AppView />
+    //   </PersistGate>
+    // </Provider>
+    <AppView />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
--- android/app/src/main/java/com/reactnativestarter/MainApplication.java
+++ android/app/src/main/java/com/reactnativestarter/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativestarter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.wumke.RNLocalNotifications.RNLocalNotificationsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNLocalNotificationsPackage(),
             new MapsPackage(),
             new RNFirebasePackage(),
             new RNFirebaseAuthPackage(),
